a6aaa8c041ce1222fe971dbfe5aa2dc00869f611
Cata R <105397119+cpazro@users.noreply.github.com>
2024-06-29 13:46:04 -0400

    Add files via upload

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,11 +5,12 @@ from django.contrib import admin
 urlpatterns = [
     path("", views.inicio, name="home"),
     path("admin/", admin.site.urls),
-    path("todos/", views.todos, name="Todos"),
+    path("carrito/todos/", views.todos, name="todos"),
     path("carrito/", views.carrito, name="carrito"),
     path("carrito/inicio/", views.inicio, name="inicio"),
     path("carrito/despliegue-producto/", views.despliegue, name="despliegue-producto"),
     path("carrito/formulario-producto/", views.formulario, name="formulario-producto"),
     path('carrito/login', views.login, name='login'),
+    
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, HttpResponse
 from .models import TodoItem
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
+@login_required
 def todos(request):
+
+    request.session ["usuario"] = "cpazro"
+    usuario=request.session.get("usuario")
+    context = {"usuario": usuario}
+
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+    return render(request, "todos.html", context)
 
+    
 def carrito(request):
     return render(request, "carrito.html")
 
@@ -23,3 +32,6 @@ def despliegue(request):
 
 def formulario(request):
     return render(request, "formulario_producto.html")
+
+def login2(request):
+    return render(request, "login2.html")
