57a622d8b09343634e627dcbcb228871332f64b8
Catalina Rodríguez <cpaz.rodriguez@uc.cl>
2024-07-01 17:30:55 -0400

    tablas cami agregadas

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import TodoItem
+from .models import TodoItem, Cliente, TipoTarjeta, Tarjeta
 
-# Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'genero', 'fecha_nacimiento', 'correo', 'es_miembro')
+    search_fields = ('nombre', 'correo')
+
+class TipoTarjetaAdmin(admin.ModelAdmin):
+    list_display = ('id_tipo', 'descripcion')
+    search_fields = ('descripcion',)
+    list_filter = ('descripcion',)  # Optional: Add filters for descriptions if there are many types
+
+class TarjetaAdmin(admin.ModelAdmin):
+    list_display = ('numero_tarjeta', 'nombre', 'tipo', 'cliente')
+    search_fields = ('numero_tarjeta', 'nombre')
+    list_filter = ('tipo',)
+
 admin.site.register(TodoItem)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(TipoTarjeta, TipoTarjetaAdmin)
+admin.site.register(Tarjeta, TarjetaAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,31 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    genero = models.CharField(max_length=1)
+    fecha_nacimiento = models.DateField()
+    clave = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    es_miembro = models.BooleanField(default=False)  # Boolean field
+
+    def __str__(self):
+        return self.nombre
+
+class TipoTarjeta(models.Model):
+    id_tipo = models.IntegerField(primary_key=True)
+    descripcion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.descripcion
+
+class Tarjeta(models.Model):
+    numero_tarjeta = models.IntegerField()
+    cvv = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    tipo = models.ForeignKey(TipoTarjeta, on_delete=models.CASCADE)  # Foreign Key to TipoTarjeta
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)  # Foreign Key to Cliente
+
+    def __str__(self):
+        return f"{self.numero_tarjeta} - {self.nombre}"
