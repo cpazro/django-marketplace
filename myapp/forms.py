864af94fa26e0c6b8cd5901c76ae36fea72ba065
Catalina Rodríguez <cpaz.rodriguez@uc.cl>
2024-07-01 17:23:10 -0400

    eliminacion archivo innecesario

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
@@ -12,3 +13,4 @@ class RegistroUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
+
